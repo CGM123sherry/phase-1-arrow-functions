f983e14fc6edc60a42235d5036fca8318dfb00af
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-03 15:32:34 +0300

    completed the arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+//divide function
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+//square function
+const square = (x) => x * x;
+//add function
+const add = (parameter1, parameter2) => parameter1 +parameter2; 
